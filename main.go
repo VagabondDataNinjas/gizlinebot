71e43830a935a1da1c655c8576cabee1b3f2b0c0
andreiashu <andreiashu@gmail.com>
2017-12-09 18:58:23 +0700

    build info

--- main.go
+++ main.go
@@ -1,9 +1,18 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/VagabondDataNinjas/gizlinebot/cmd"
 )
 
+var (
+	version = "dev"
+	commit  = "none"
+	date    = "unknown"
+)
+
 func main() {
+	fmt.Printf("\nGROOTs linebot %v, commit %v, built at %v\n", version, commit, date)
 	cmd.Execute()
 }
